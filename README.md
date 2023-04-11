18946a3a2a43e89311b003bfe2c96ff7e277c676
Alex Cabrera <alex.cabrera@gmail.com>
2023-04-11 18:55:40 -0400

    refactor for standard cases

--- README.md
+++ README.md
@@ -2,10 +2,9 @@
 
 Use [Zeno](https://github.com/zeno-ml/zeno) to visualize the results of [OpenAI Evals](https://github.com/openai/evals/blob/main/docs/eval-templates.md).
 
-
 https://user-images.githubusercontent.com/4563691/225655166-9fd82784-cf35-47c1-8306-96178cdad7c1.mov
 
-*Example using `zeno-evals` to explore the results of an OpenAI eval on multiple choice medicine questions (MedMCQA)*
+_Example using `zeno-evals` to explore the results of an OpenAI eval on multiple choice medicine questions (MedMCQA)_
 
 ### Usage
 
@@ -23,13 +22,6 @@ zeno-evals /tmp/evallogs/my_eval_cache.jsonl
 
 ### Example
 
-We include an example looking at the [MedMCQA](https://github.com/openai/evals/pull/141) dataset (Thanks to @SinanAkkoyun):
-
 ```bash
-zeno-evals ./example_medicine/example.jsonl --functions_file=./example_medicine/distill.py
+zeno-evals ./example/example.jsonl
 ```
-
-### Todo
-
-- [ ] Support model-graded evaluations
-- [ ] Support custom evaluation templates (e.g. BLEU for translation)
--- zeno_evals/main.py
+++ zeno_evals/main.py
@@ -9,8 +9,8 @@ from importlib import util
 
 import fire
 import pandas as pd
-from zeno import DistillReturn, ModelReturn, distill, metric, model, zeno
-from zeno.api import DistillReturn, MetricReturn, ZenoOptions
+from zeno import ModelReturn, metric, model, zeno
+from zeno.api import MetricReturn, ZenoOptions
 
 
 @contextmanager
@@ -56,148 +56,141 @@ def normalize(s: str) -> str:
     return s
 
 
+def get_metric_function(metric_name):
+    def metric_function(df, ops: ZenoOptions):
+        if len(df) == 0:
+            return MetricReturn(metric=0.0)
+        if (
+            df[metric_name].dtype == "object"
+            and df[metric_name].value_counts().shape[0] <= 2
+        ):
+            return MetricReturn(
+                metric=df[metric_name].eq(df[metric_name][0]).mul(1).mean()
+            )
+        return MetricReturn(metric=df[metric_name].mean())
+
+    metric_function.__name__ = metric_name
+
+    return metric(metric_function)
+
+
 @model
 def model_fn(name):
     def mod(df, ops):
-        return ModelReturn(model_output=df["sample"])
+        return ModelReturn(model_output=df["sampled"])
 
     return mod
 
 
-@distill
-def match(df, ops):
-    matches = []
-    for i, row in df.iterrows():
-        if type(row["ideal"]) == list:
-            matches.append(
-                any(
-                    normalize(b).startswith(normalize(row[ops.output_column]))
-                    for b in row["ideal"]
-                )
-            )
-        else:
-            matches.append(
-                normalize(row["ideal"]).startswith(normalize(row[ops.output_column]))
-            )
-    return DistillReturn(distill_output=matches)
+@metric
+def correct(df, ops: ZenoOptions):
+    return MetricReturn(metric=df["correct"].astype(int).mean() * 100)
 
 
-@distill
-def includes(df, ops):
-    matches = []
-    for i, row in df.iterrows():
-        if type(row["ideal"]) == list:
-            matches.append(
-                any(
-                    normalize(row[ops.output_column]) in normalize(b)
-                    for b in row["ideal"]
-                )
-            )
-        else:
-            matches.append(normalize(row[ops.output_column]) in normalize(row["ideal"]))
-    return DistillReturn(distill_output=matches)
+def read_results_file(data):
+    sampling_df = pd.DataFrame(
+        [
+            {
+                "id": d["sample_id"],
+                "prompt": d["data"]["prompt"],
+                "sampled": d["data"]["sampled"][0],
+            }
+            for d in data[2:]
+            if "type" in d and d["type"] == "sampling"
+        ]
+    )
+    match_df = pd.DataFrame(
+        [
+            {
+                "id": d["sample_id"],
+                "correct": d["data"]["correct"],
+                "expected": d["data"]["expected"],
+            }
+            for d in data[2:]
+            if "type" in d and d["type"] == "match"
+        ]
+    )
+
+    metric_names = []
+    for d in data[2:]:
+        if "type" in d and d["type"] == "metrics":
+            metric_names = list(d["data"].keys())
+            break
+
+    metrics = []
+    for d in data[2:]:
+        if "type" in d and d["type"] == "metrics":
+            met_obj = {"id": d["sample_id"]}
+            for name in metric_names:
+                met_obj[name] = d["data"][name]
+            metrics.append(met_obj)
+    metrics_df = pd.DataFrame(metrics)
+
+    df = sampling_df
+    if len(match_df) > 0:
+        df = df.join(match_df.set_index("id"), on="id")
+    if len(metrics_df) > 0:
+        df = df.join(metrics_df.set_index("id"), on="id")
+
+    return df, metric_names
 
 
-@distill
-def fuzzy_match(df, ops):
-    matches = []
-    for i, row in df.iterrows():
-        if type(row["ideal"]) == list:
-            matches.append(
-                any(
-                    normalize(b) in normalize(row[ops.output_column])
-                    or normalize(row[ops.output_column]) in normalize(b)
-                    for b in row["ideal"]
-                )
-            )
-        else:
-            matches.append(normalize(row[ops.output_column]) in normalize(row["ideal"]))
-    return DistillReturn(distill_output=matches)
-
-
-def get_match_function(template):
-    if template == "match":
-        return match
-    elif template == "includes":
-        return includes
-    elif template == "fuzzy_match":
-        return fuzzy_match
-    else:
-        raise ValueError("Invalid template: {}".format(template))
-
-
-def get_accuracy_function(template):
-    @metric
-    def accuracy(df, ops: ZenoOptions):
-        m = df[ops.distill_columns[template]].astype(int).mean()
-        if pd.notna(m):
-            return MetricReturn(metric=m * 100)
-        return MetricReturn(metric=0)
-
-    return accuracy
-
-
-def main(file: str, template: str = "match", functions_file: str = None):
+def main(
+    results_file: str, second_results_file: str = None, functions_file: str = None
+):
     """Visualize a result from OpenAI evals using Zeno.
 
     Args:
-            file (path): Result .jsonl file from OpenAI evals. Often stored in the /tmp/evallogs/ directory.
+            results_file (path): Result .jsonl file from OpenAI evals. Often stored in the /tmp/evallogs/ directory.
 
-            template (str, optional): Evaluation template to use. Can be "match", "includes" or "fuzzy_match". Defaults to "match".
+            second_results_file (path): Second result .jsonl file from OpenAI evals for comparison. Often stored in the /tmp/evallogs/ directory.
 
             functions_file (path, optional): Path to a Python file containing additional Zeno processing functions. Defaults to None.
     """
 
-    if not os.path.exists(file):
-        print("ERROR: file '{}' does not exist.".format(file))
+    if not os.path.exists(results_file):
+        print("ERROR: file '{}' does not exist.".format(results_file))
         sys.exit(1)
 
     data = []
-    with open(file) as f:
+    with open(results_file) as f:
         for line in f:
             data.append(json.loads(line))
 
-    raw_samples = [
-        {"input": d["data"]["input"], "id": d["sample_id"], "ideal": d["data"]["ideal"]}
-        for d in data[2:]
-        if "type" in d and d["type"] == "raw_sample"
-    ]
-    samples = [
-        {"id": d["sample_id"], "sample": d["data"]["sampled"]}
-        for d in data[2:]
-        if "type" in d and d["type"] == "sampling"
-    ]
+    df, metric_names = read_results_file(data)
 
-    df_samples = pd.DataFrame(samples)
-    df = pd.DataFrame(raw_samples)
-    df = df.join(df_samples.set_index("id"), on="id")
-
-    extra_fns = []
+    functions = [model_fn]
     if functions_file is not None:
-        extra_fns = parse_testing_file(functions_file)
+        functions = functions + parse_testing_file(functions_file)
 
-    zeno(
-        {
-            "metadata": df,
-            "models": [data[0]["spec"]["model_name"]],
-            "functions": [
-                model_fn,
-                get_accuracy_function(template),
-                get_match_function(template),
-            ]
-            + extra_fns,
-            "view": "openai-chat",
-            "data_column": "input",
-            "id_column": "id",
-            "label_column": "ideal",
-            "cache_path": "./.zeno_cache_"
-            + template
-            + os.path.basename(file).split("_")[0],
-            "port": 8080,
-            "batch_size": 100,
-        }
-    )
+    for m in metric_names:
+        functions = functions + [get_metric_function(m)]
+
+    if "expected" in df.columns:
+        functions = functions + [correct]
+
+    zeno_config = {
+        "metadata": df,
+        "models": data[0]["spec"]["completion_fns"],
+        "functions": functions,
+        "view": "openai-chat",
+        "data_column": "prompt",
+        "id_column": "id",
+        "cache_path": "./.zeno_cache_" + os.path.basename(results_file).split("_")[0],
+        "port": 8080,
+        "batch_size": 100,
+        "samples": 5,
+    }
+
+    if "expected" in df.columns:
+        zeno_config["label_column"] = "expected"
+
+    zeno(zeno_config)
 
 
 def cli():
+    fire.Fire(main)
+
+
+if __name__ == "__main__":
     fire.Fire(main)
